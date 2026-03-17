6d5781db378f759e472cc964d48640125b4d0401
HUGO DE OLIVEIRA ALVES VIEIRA <25203638@CFP127ALUNO.LOCAL>
2026-03-17 09:38:48 -0300

    atividade 2 concluida

--- 02-calculadora-desconto/modulo/app.js
+++ 02-calculadora-desconto/modulo/app.js
@@ -1,23 +1,60 @@
 'use stric'
 
+
 function removerClasses () {
     const resultado = document.getElementById('resultado')
-    resultado.classList.remove('apto', 'naoApto')
+    resultado.classList.remove('cinco', 'dez', 'acimaDez')
 }
 
-function calculo (){
-    const precoOriginal      = document.getElementById('original')
-    const percentualDesconto = document.getElementById('desconto')
+//  vai fazer o calculo desconto
+function calcularDesconto (){
+    const precoOriginal      = document.getElementById('preco').value
+    const percentualDesconto = document.getElementById('desconto').value
     
-    const resultado = (percentualDesconto / 100) * precoOriginal
+    const valorEco = (Number(percentualDesconto) / 100) * Number(precoOriginal)
 
-    imprimirResultado(resultado)
-
+    trocaDeCor(percentualDesconto)
+    calcularPrecoFinal(precoOriginal, valorEco)
 }
 
-function imprimirResultado (valor1) {
+// vai fazer o calculo do valor final
+function calcularPrecoFinal (precoInicial, precoFinal){
+    let precoI = precoInicial
+    let precoF = precoFinal
 
-    const resultado = valor1
+    let resultCalc = Number(precoI) - Number(precoF)
+
+    exibirResultado(precoF, resultCalc)
+}
+
+// vai exibir os resultaos na tbl de resultado
+function exibirResultado (desconto, precoFinal){
+    const result             = document.getElementById('resultado')
+    let preco    = precoFinal
+    let economia = desconto
+
+    result.textContent = `Você vai economizar: ${Number(economia).toFixed(2)} - O valor final será: ${Number(preco).toFixed(2)}`
+}
+
+// troca a cor da borda da resposta
+const trocaDeCor = function(percentual){
+    const resultado  = document.getElementById('resultado')
+    const desconto   = Number(percentual)
+
+    // exclui a classe criada
+    resultado.classList.remove('cinco', 'dez', 'acimaDez')
+
+    if (desconto <= 5){
+        resultado.classList.add('cinco')
+
+    }else if (desconto > 5 && desconto < 10){
+        resultado.classList.add('dez')
+
+    }else if (desconto > 10){
+        resultado.classList.add('acimaDez')
+    }
+
+}
 
     
-}
+
