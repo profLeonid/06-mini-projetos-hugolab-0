ba214375ee379b0350df501c86e686b69e77b754
HUGO DE OLIVEIRA ALVES VIEIRA <25203638@CFP127ALUNO.LOCAL>
2026-03-24 08:34:17 -0300

    trabalho completo

--- 04-calculadora-imc/mudulo/app.js
+++ 04-calculadora-imc/mudulo/app.js
@@ -1,5 +1,52 @@
 'use stric'
 
-function calculoImc (){}
+function calculoIMC(){
+    let n1 = document.getElementById('altura').value
+    let n2 = document.getElementById('peso').value
 
-function exibirResultadoECor (){}
+    let altura = Number(n1)
+    let peso = Number(n2)
+
+    let imc = peso / (altura * altura)
+
+    console.log(imc)
+
+    exibirResultado(imc)
+}
+function exibirResultado(valor1){
+    let result = document.getElementById('resultado')
+    let imc = Number(valor1)
+
+    result.classList.remove('baixo', 'normal', 'sobrepeso', 'grauI', 'grauII', 'grauIII')
+
+    if (imc < 18.5){
+        console.log('Abaixo do peso')
+        result.classList.add('baixo')
+    }
+    else if(imc >= 18.5 && imc < 24.9){
+        console.log('Peso normal')
+        result.classList.add('normal')
+    }
+
+    else if(imc >= 25.0 && imc < 29.9){
+        console.log('Sobrepeso')
+        result.classList.add('sobrepeso')
+    }
+
+    else if(imc >= 30.0 && imc < 34.9){
+        console.log('Obesidade grau I')
+        result.classList.add('grauI')
+    }
+
+    else if(imc >= 35.0 && imc < 39.9){
+        console.log('Obesidade grau II')
+        result.classList.add('grauII')
+    }
+
+    else if(imc >= 40.0){
+        console.log('Obesidade grau III')
+        result.classList.add('grauIII')
+    }
+}
+
+function mudarCor(){}
